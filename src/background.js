d97d3c229cea6f6cee58dd319d22c999d3388e3c
Chevy Li <Chevy.LI@cn.bosch.com>
2023-06-02 14:43:24 +0800

    migrate existing code

--- src/background.js
+++ src/background.js
@@ -19,3 +19,44 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   }
 });
+
+async function setSelectedTextBackgroundColor(tabUrl) {
+  console.debug(tabUrl);
+
+  const span = document.createElement("span");
+  span.style.backgroundColor = "rgba(144, 238, 144)"; // Set background color to yellow
+  // span.style.padding = "0.2em"; // Add padding for a marker-like effect
+  span.style.borderRadius = "0.2em"; // Add rounded corners for a marker-like effect
+  span.style.boxShadow = "0 0 2px 2px rgba(144, 238, 144, 0.8)"; // Add a subtle shadow
+
+  const range = window.getSelection().getRangeAt(0);
+  var selectionContents = range.extractContents();
+  console.debug(selectionContents.textContent);
+  span.innerText = selectionContents.textContent;
+  range.insertNode(span);
+  
+  const currentHighlights = await chrome.storage.local.get(tabUrl);
+  if (!currentHighlights[tabUrl]) {
+    currentHighlights[tabUrl] = [];
+  }
+  currentHighlights[tabUrl].push(selectionContents.textContent);
+  
+  chrome.storage.local.set({
+    [tabUrl]: currentHighlights[tabUrl]
+  });
+}
+
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "highlightTexts") {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: setSelectedTextBackgroundColor,
+      args: [tab.url],
+    });
+  }
+});
+
