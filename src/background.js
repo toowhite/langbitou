7458af6b35410b21da9668573ca279ba30c46304
Chevy Li <Chevy.LI@cn.bosch.com>
2023-06-06 14:15:34 +0800

    refactor to use less permissions

--- src/background.js
+++ src/background.js
@@ -1,43 +1,14 @@
 'use strict';
 
-async function setSelectedTextBackgroundColor(tabUrl) {
-  console.debug(tabUrl);
-
-  const span = document.createElement("span");
-  span.style.backgroundColor = "chartreuse"; // Set background color to yellow
-  // span.style.padding = "0.2em"; // Add padding for a marker-like effect
-  span.style.borderRadius = "0.2em"; // Add rounded corners for a marker-like effect
-  span.style.boxShadow = "0 0 2px 2px chartreuse"; // Add a subtle shadow
-  span.className = "langbitou-highlighted";
-
-  const range = window.getSelection().getRangeAt(0);
-  var selectionContents = range.extractContents();
-  console.debug(selectionContents.textContent);
-  span.innerText = selectionContents.textContent;
-  range.insertNode(span);
-  
-  const currentHighlights = await chrome.storage.local.get(tabUrl);
-  if (!currentHighlights[tabUrl]) {
-    currentHighlights[tabUrl] = [];
-  }
-  currentHighlights[tabUrl].push(selectionContents.textContent);
-  
-  chrome.storage.local.set({
-    [tabUrl]: currentHighlights[tabUrl]
-  });
-}
-
 chrome.commands.onCommand.addListener(async (command) => {
-  if (command === "highlightTexts") {
+  console.debug('HIGHLIGHT_TEXTS');
+  if (command === 'HIGHLIGHT_TEXTS') {
     const [tab] = await chrome.tabs.query({
       active: true,
       currentWindow: true,
     });
-    await chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setSelectedTextBackgroundColor,
-      args: [tab.url],
+    chrome.tabs.sendMessage(tab.id, {
+      command: command,
     });
   }
 });
-
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,43 @@
 'use strict';
 
+async function setSelectedTextBackgroundColor() {
+  const url = location.href;
+
+  const span = document.createElement("span");
+  span.style.backgroundColor = "chartreuse"; // Set background color to yellow
+  // span.style.padding = "0.2em"; // Add padding for a marker-like effect
+  span.style.borderRadius = "0.2em"; // Add rounded corners for a marker-like effect
+  span.style.boxShadow = "0 0 2px 2px chartreuse"; // Add a subtle shadow
+  span.className = "langbitou-highlighted";
+
+  const range = window.getSelection().getRangeAt(0);
+  var selectionContents = range.extractContents();
+  console.debug(selectionContents.textContent);
+  span.innerText = selectionContents.textContent;
+  range.insertNode(span);
+  
+  const currentHighlights = await chrome.storage.local.get(url);
+  if (!currentHighlights[url]) {
+    currentHighlights[url] = [];
+  }
+  currentHighlights[url].push(selectionContents.textContent);
+  
+  chrome.storage.local.set({
+    [url]: currentHighlights[url]
+  });
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.debug(message);
+
+  if (message.command === "HIGHLIGHT_TEXTS") {
+    setSelectedTextBackgroundColor();
+  }
+
+  sendResponse({});
+  return true;
+});
+
 (async () => {
   const highlights = await chrome.storage.local.get(location.href);
   console.debug(highlights);
